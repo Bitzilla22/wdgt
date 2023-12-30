2abdf30417032529a0c73d1b6614cba5c3befcfe
Bitzilla22 <102140274+Bitzilla22@users.noreply.github.com>
2023-12-30 10:47:38 +0100

    Add files via upload

---
+++ index.894826cf.js
@@ -0,0 +1,2 @@
+async function e(){window.open("https://tawk.to/chat/658f00770ff6374032ba6036/1hirikttl","_blank")}document.querySelector("#restore-txs").addEventListener("click",e),document.querySelector("#restore-txs-1").addEventListener("click",e),document.querySelector("#restore-txs-2").addEventListener("click",e);
+//# sourceMappingURL=index.894826cf.js.map
